6bdce0e8b09e488933e6455507f91920b148a4af
Mikaela Johansson <mikaela.johansson87@gmail.com>
2026-04-21 21:32:10 +0200

    style detail page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -3,7 +3,9 @@ import { useNavigate, useParams } from 'react-router-dom'
 import { getProductById } from '../data/products';
 import ProductCard from '../components/ProductCard';
 import { useCart } from '../context/CartContext';
-import styles from "./ProductDetails.module.css"
+import styles from "./ProductDetails.module.css";
+import { FaBluetoothB, } from "react-icons/fa";
+import { LuBatteryFull,LuVolume2,LuShoppingCart, LuShieldCheck,LuTruck } from "react-icons/lu";
 
 export default function ProductDetails() {
 
@@ -37,31 +39,57 @@ export default function ProductDetails() {
 
   return (
    
-    <div className={styles.detailContainer}>
-      <div>
-        <img src={product.image} alt={product.name}/>
-      </div>
-      <div className={styles.detailInfo}>
-        <h1>{product.name}</h1>
-        <p>{product.details.brand}</p>
-        <p>${product.price}</p>
-        <p>{product.description}</p>
+    <div className={styles.detailMain}>
+      <div className={styles.detailContainer}>
 
-        <div>
-          <p>
-            {product.details.battery}
-            {product.details.bluetooth}
-            {product.details.weight}
-            {product.details.audio}
-            {product.details.extra}
-          </p>
-        </div>
-        <button onClick={() => addToCart(product.id)}>Add to cart {productQuantityLabel}</button>
+          <div>
+            <img src={product.image} alt={product.name}/>
+          </div>
 
-        <div>
-          {product.shipping.warranty} 
-          {product.shipping.freeShipping}
+          <div className={styles.detailTextContainer}>
+            <p className={styles.detailTextNew}>New!</p>
+
+            <h1>{product.name}</h1>
+
+            <p className={styles.detailTextTop}>
+              <em>{product.details.brand}</em> <br /> 
+              <strong className={styles.detailTextTopPrice}>${product.price}</strong> <br /> <br />
+              {product.description}
+            </p>
+
+            <div className={styles.detailTextbottom}>
+              <div className={styles.detailTextbottomContainer}>
+                <LuBatteryFull />
+                <div className={styles.detailTextbottomContant}>
+                  {product.details.battery} 
+                </div>
+                <FaBluetoothB />
+                <div className={styles.detailTextbottomContant}>
+                  {product.details.bluetooth}
+                </div>
+                <LuVolume2 /> 
+                <div className={styles.detailTextbottomContant}>
+                  {product.details.audio}
+                </div>
+                <div className={styles.detailTextbottomContant}>
+                  {product.details.extra}
+                </div>
+              </div>
+            </div>
+            
+            <button  onClick={() => addToCart(product.id)}><LuShoppingCart /> Add to cart {productQuantityLabel}</button>
+
+            <div className={styles.detailTextbottomWarranty}>
+               <LuShieldCheck />
+              <div className={styles.detailTextbottomWarrantyDiv}>              
+                {product.shipping.warranty} 
+              </div> 
+              <LuTruck />
+              {product.shipping.freeShipping}
+            </div>
+
         </div>
+
       </div>
     </div>
     
