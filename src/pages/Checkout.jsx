85be16e98530a0f263de33ab1139dd1e0797237a
Mikaela Johansson <mikaela.johansson87@gmail.com>
2026-04-22 16:20:11 +0200

    style fix

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,10 +1,12 @@
+import styles from "./Checkout.module.css"
 import { useCart } from '../context/CartContext';
+import { useNavigate } from 'react-router-dom'
 
 export default function Checkout() {
 
+  const navigate = useNavigate();
   const { getCartItemsWithProducts, removeFromCart, updateQuantity, getCartTotal, clearCart } = useCart();
   const cartItems = getCartItemsWithProducts();
-
   const total = getCartTotal()
 
   function placeOrder(){
@@ -14,57 +16,70 @@ export default function Checkout() {
 
   return (
     <div>
-      <div>
-        <h1>Checkout</h1>
-        <div>
-          <div>
+      <div className={styles.CheckoutMain}>
+        <h1>Shopping Cart</h1>
+        <div className={styles.CheckoutContainer}>
 
-            <h2>Order summary</h2>
-
+          <div >
             {cartItems.map((item) => (
-              <div key={item.id}>
+              <div key={item.id} className={styles.CheckoutProductsInCart}>
+                
+                <img
+                  className={styles.CheckoutImage}
+                  src={item.product.image}
+                  alt={item.product.name}
+                />
 
-                <img src={item.product.image} alt={item.product.name} />
-
-                <div>
+                <div className={styles.CheckoutProductName}>
                   <h3>{item.product.name}</h3>
-                  <p>${item.product.price} each </p>
+                  <p>${item.product.price} each</p>
                 </div>
 
-                <div>
-                  <div>
-                    <button onClick={() => updateQuantity(item.id, item.quantity -1)}>-</button>
+                <div className={styles.CheckoutActions}>
+                  <p>${(item.product.price * item.quantity).toFixed(2)}</p>
+
+                  <div className={styles.QuantityControls}>
+                    <button onClick={() => updateQuantity(item.id, item.quantity - 1)}>-</button>
                     <span>{item.quantity}</span>
-                    <button onClick={() => updateQuantity(item.id, item.quantity +1)}>+</button>
+                    <button onClick={() => updateQuantity(item.id, item.quantity + 1)}>+</button>
                   </div>
 
-                  <p>
-                    ${(item.product.price * item.quantity).toFixed(2)}
-                  </p>
-
-                  <button onClick={() => removeFromCart(item.id)}>Remove</button>
-
+                  <button className={styles.CheckoutButtonRemove} onClick={() => removeFromCart(item.id)}>Remove</button>
                 </div>
-
               </div>
             ))}
-
           </div>
-          <div>
-            <h2>Total</h2>
-            <div>
-              <p>Subtotal:</p>
-              <p>${total.toFixed(2)}</p>
+
+          <div className={styles.checkoutPayContainer}>
+            <div className={styles.checkoutSummary}>
+              <div className={styles.checkoutRow}>
+                <p>Subtotal:</p>
+                <p>${total.toFixed(2)}</p>
+              </div>
+
+              <div className={styles.checkoutRowShipping}>
+                <p>Shipping:</p>
+                <p>Free</p>
+              </div>
+
+              <div className={styles.checkoutRow}>
+                <p>Total:</p>
+                <p>${total.toFixed(2)}</p>
+              </div>
             </div>
-            <div>
-              <p>Total:</p>
-              <p>${total.toFixed(2)}</p>
-            </div>
-            <button onClick={placeOrder}>
+          </div>
+
+          <div className={styles.checkoutButtonCheckoutContainer}>
+
+            <button className={styles.checkoutButtonContinueShopping} onClick={( () => navigate("/"))}>Continue Shopping</button>
+
+            <button className={styles.checkoutButtonPlaceOreder} onClick={placeOrder}>
               Place order
             </button>
 
+            <br />
           </div>
+
         </div>
       </div>
     </div>
